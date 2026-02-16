69ae8f96a787235d8e85094980cdf28636164aec
book27426 <51449147+book27426@users.noreply.github.com>
2026-02-16 14:25:15 +0700

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,8 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
+const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
-export default pool;
+export { db };
