7d06c0ca4959d24bd5fecc16b4b4df89ce2601a4
Npass <b.book0601@gmail.com>
2026-02-18 10:59:44 +0700

    uodate18/2fixerrorinuserid

--- src/app/api/v1/user/[id]/route.js
+++ src/app/api/v1/user/[id]/route.js
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
-export async function GET(_, { params }) {
+export async function GET(_, context) {
+  const { id } = await context.params;
   const { rows } = await db.query(
     `SELECT * FROM users WHERE id=$1 AND is_deleted=false`,
-    [params.id]
+    [id]
   );
 
   if (!rows.length)
